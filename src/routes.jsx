6e7dd999a848f6a378e07729876582436111b8e4
Farhiya 💫 <fbhy21@gmail.com>
2024-11-11 21:14:46 +0300

    statsheader component (#16)

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,7 @@
 import App from "/src/App.jsx";
 import Home from "/src/pages/Home.jsx";
 import CrazyNotFoundPage from "/src/pages/CrazyNotFoundPage";
-
-
+import StatsHeader from "/src/components/ui/StatsHeader"; // Updated component name
 
 const routes = [
   {
@@ -11,9 +10,9 @@ const routes = [
     errorElement: <CrazyNotFoundPage />,
     children: [
       { path: "/", element: <Home /> },
-        ],
+      { path: "/stats", element: <StatsHeader /> }, // Route for StatsHeader
+    ],
   },
 ];
-
 
 export default routes;
